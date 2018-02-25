ff1326e90700d89988e16236b29c4bd501954d15
Feng Ya <fengya04@126.com>
2018-02-25 11:27:10 +0800

    input bottom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-console.log("Incremental Reading for Language Learning.");
+console.log('Incremental Reading for Language Learning.');
 
 const highlight = {
   add() {
-    document.querySelectorAll("span.highlight").forEach(highlight => {
-      const id = highlight.getAttribute("data-api-id");
+    document.querySelectorAll('span.highlight').forEach(highlight => {
+      const id = highlight.getAttribute('data-api-id');
       highlight.insertAdjacentHTML(
-        "beforebegin",
+        'beforebegin',
         `<input class="cloze" data-api-id="${id}" type="text" style="width: ${
           highlight.offsetWidth
         }px" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">`
@@ -13,36 +13,36 @@ const highlight = {
     });
   },
   toggle() {
-    document.querySelectorAll("span.highlight").forEach(highlight => {
-      highlight.classList.toggle("mask");
+    document.querySelectorAll('span.highlight').forEach(highlight => {
+      highlight.classList.toggle('mask');
     });
-  }
+  },
 };
 
 const cloze = {
   add() {
-    document.querySelectorAll(".cloze").forEach(cloze => {
-      const id = cloze.getAttribute("data-api-id");
+    document.querySelectorAll('.cloze').forEach(cloze => {
+      const id = cloze.getAttribute('data-api-id');
       const answer = document.querySelector(
         `span.highlight[data-api-id="${id}"]`
       );
-      cloze.addEventListener("keypress", event => {
-        answer.classList.remove("wrong");
-        answer.classList.remove("correct");
+      cloze.addEventListener('keypress', event => {
+        answer.classList.remove('wrong');
+        answer.classList.remove('correct');
 
         // Enter or Tab
         if (event.keyCode === 13 || event.keyCode === 9) {
-          if (cloze.value !== answer.textContent) answer.classList.add("wrong");
-          else answer.classList.add("correct");
+          if (cloze.value !== answer.textContent) answer.classList.add('wrong');
+          else answer.classList.add('correct');
         }
       });
     });
   },
   clear() {
-    document.querySelectorAll(".cloze").forEach(cloze => {
+    document.querySelectorAll('.cloze').forEach(cloze => {
       cloze.parentNode.removeChild(cloze);
     });
-  }
+  },
 };
 
 // remove old clozes (used only in development)
@@ -54,11 +54,11 @@ cloze.add();
 
 let clozeMode = true;
 
-document.body.addEventListener("click", event => {
-  if (event.target.nodeName !== "BODY") return;
+document.body.addEventListener('click', event => {
+  if (event.target.nodeName !== 'BODY') return;
 
   // Instapaper: Remove Note
-  if (document.querySelectorAll("div.highlight_popover.reveal").length > 0)
+  if (document.querySelectorAll('div.highlight_popover.reveal').length > 0)
     return;
 
   if (clozeMode) {
@@ -71,4 +71,81 @@ document.body.addEventListener("click", event => {
   highlight.toggle();
 
   clozeMode = !clozeMode;
+});
+
+const highlights = document.querySelectorAll('span.highlight');
+let pointer;
+
+function prev() {
+  highlights.forEach(highlight => {
+    highlight.classList.remove('answer');
+  });
+
+  pointer = (pointer - 1 + highlights.length) % highlights.length;
+
+  console.log(pointer);
+
+  const highlight = highlights[pointer];
+  highlight.classList.add('answer');
+  highlight.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'center',
+  });
+}
+
+function next() {
+  highlights.forEach(highlight => {
+    highlight.classList.remove('answer');
+  });
+
+  pointer === undefined
+    ? (pointer = 0)
+    : (pointer = (pointer + 1) % highlights.length);
+
+  console.log(pointer);
+
+  const highlight = highlights[pointer];
+  highlight.classList.add('answer');
+  highlight.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center',
+    inline: 'center',
+  });
+}
+
+next();
+
+// clear #instalang
+document.querySelectorAll('#instalang').forEach(instalang => {
+  instalang.parentNode.removeChild(instalang);
+});
+
+document
+  .querySelector('.container')
+  .insertAdjacentHTML(
+    'afterend',
+    '<div id="instalang"><input id="cloze" type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" /></div>'
+  );
+
+function check() {
+  const input = document.querySelector('#cloze');
+  console.log(input.value);
+  return input.value === highlights[pointer].textContent;
+}
+
+document.querySelector('#cloze').addEventListener('keypress', event => {
+  if (event.key === 'Tab') {
+    console.log('Tab');
+    if (check()) {
+      event.shiftKey ? prev() : next();
+    }
+    event.preventDefault();
+  }
+  if (event.key === 'Enter') {
+    console.log('Enter');
+    if (check()) {
+      event.shiftKey ? prev() : next();
+    }
+  }
 });
